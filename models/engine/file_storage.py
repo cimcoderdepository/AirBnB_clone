89606d6bc8b43890878c088168bd61d6075c0830
cimcoderdepository <gloryland4ever@gmail.com>
2023-05-14 21:45:27 +0100

    commit task 4,5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,49 @@
 #!/usr/bin/python3
+"""Serialize instances to JSON file and deserialize JSON file to instances."""
 import json
+from models import base_model
 
-class Filestorage():
+
+class FileStorage:
+    """Initialize a class FileStorage.
+
+    Attributes:
+                @__file_path:   # a JSON file to log every instances per call.
+
+                @__objects:     # an empty dictionary but will store all
+                                # objects each time an instance is called
+    """
+
     __file_path = "file.json"
-    __object = {}
+    __objects = {}
 
     def all(self):
+        """Instance method that return the dictionary."""
         return self.__objects
 
     def new(self, obj):
-        key = f"{self.__class__.__name__}.{self.id}"
-        self.__object[key] = obj
+        """Set empty dictionary to populate the values to keys.
+
+        key:    # dictionary key
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
-        with open(self.__file_path, "w",encoding="utf-8") as f:
-            d = {k : v.to_dict for k, v in self.__object.items()}
+        """Serialize dictionary to the JSON file."""
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(d, f)
 
     def reload(self):
+        """Deserialize the JSON file to dictionary."""
         try:
             with open(self.__file_path, "r", encoding="utf-8") as f:
                 obj_dict = json.load(f)
-                obj_dict = eval(self.__class__.__name__(obj_dict))
+                for o in obj_dict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(f"base_model.{cls_name}")(**o))
 
-        except:
-            pass
-            
-
+        except FileNotFoundError:
+            return
